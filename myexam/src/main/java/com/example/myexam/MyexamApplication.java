2bf9f028e2203a4970a4fa81be07f838249de307
smdimran2000 <shaikmdimran31@gmail.com>
2023-02-14 12:29:03 +0530

    hd

---
+++ myexam/src/main/java/com/example/myexam/MyexamApplication.java
@@ -0,0 +1,13 @@
+package com.example.myexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyexamApplication.class, args);
+	}
+
+}
---
+++ myexam/src/test/java/com/example/myexam/MyexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
